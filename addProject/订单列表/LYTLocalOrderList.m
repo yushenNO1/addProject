2c7d7a3439342d51b78b56f7c52b69efc705dc65
yuting <1094117660@qq.com>
2017-08-12 08:35:33 +0800

    8.11

--- addProject/订单列表/LYTLocalOrderList.m
+++ addProject/订单列表/LYTLocalOrderList.m
@@ -38,9 +38,6 @@ static NSString *DefaultCell            = @"DefaultCell";
 
 
 
-
-
-
 #pragma mark--------tableViewDataSource--------
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     return 2;
@@ -53,15 +50,5 @@ static NSString *DefaultCell            = @"DefaultCell";
     return cell;
 }
 
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
